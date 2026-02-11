bf12877dc1a428e5c6e2dccac80cc62af2c4027e
Cristian Muñoz <cristian.munoz@pragma.com.co>
2026-02-11 15:30:17 -0500

    Create ApplicationConfig and finish security config

--- src/main/java/com/pragma/usersMicroservice/infrastructure/security/config/ApplicationConfig.java
+++ src/main/java/com/pragma/usersMicroservice/infrastructure/security/config/ApplicationConfig.java
@@ -1,17 +1,19 @@
 package com.pragma.usersMicroservice.infrastructure.security.config;
 
-import com.pragma.usersMicroservice.infrastructure.output.jpa.entity.UserEntity;
 import com.pragma.usersMicroservice.infrastructure.output.jpa.mapper.IUserEntityMapper;
 import com.pragma.usersMicroservice.infrastructure.output.jpa.repository.IUserRepository;
 import com.pragma.usersMicroservice.infrastructure.security.adapter.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ public class ApplicationConfig {
 
     private final IUserRepository userRepository;
     private final IUserEntityMapper userEntityMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -26,5 +29,17 @@ public class ApplicationConfig {
                 .map(userEntityMapper::toUser)
                 .map(CustomUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/pragma/usersMicroservice/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/pragma/usersMicroservice/infrastructure/security/config/SecurityConfig.java
@@ -1,11 +1,45 @@
 package com.pragma.usersMicroservice.infrastructure.security.config;
 
+import com.pragma.usersMicroservice.infrastructure.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        http
+                //Disable CSRF
+                .csrf(AbstractHttpConfigurer::disable)
+                //Allowed origins
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                    //Allow all http request to auth endpoints
+                        authorizeHttpRequests
+                        .requestMatchers("/auth")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
